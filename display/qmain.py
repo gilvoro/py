70e0ad44ea807b01c457acf4613eba4a85c7263b
gilvoro <gilvoro@users.noreply.github.com>
2019-11-28 14:54:22 -0700

    uploaded the actually qmain file

--- display/qmain.py
+++ display/qmain.py
@@ -2,5 +2,16 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import font
 
+
+
+dirloc = 'C:\\pve\\qd\\testfiles'
+dfiles = ['skquotes','scquotes']
+
 def quit(*args):
   root.destroy()
+
+root = Tk()
+root.attributes("-fullscreen", True)
+root.configure(background='black')
+root.bind("<Escape>", quit)
+root.bind("x", quit)
